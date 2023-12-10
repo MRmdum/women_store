7d26cd856171ee049c3bee830972de2891da9526
Pierre Olivier COUDERT <pierreolivier.coudert@laposte.net>
2023-12-10 12:47:31 +0100

    bddV1

--- sql/premierFhichierSQL.sql
+++ sql/premierFhichierSQL.sql
@@ -2,34 +2,23 @@ DROP DATABASE IF EXISTS women_store;
 CREATE DATABASE IF NOT EXISTS women_store;
 USE women_store;
 
-#User root/root
-
 #Création des tables
 
 CREATE TABLE client (
   id_client INT NOT NULL AUTO_INCREMENT,
-  email_client VARCHAR(255) NOT NULL UNIQUE,
   nom_client VARCHAR(255) NOT NULL,
   prenom_client VARCHAR(255) NOT NULL,
-  telephone_client VARCHAR(255) NOT NULL, #VARCHAR car pas d'intérêt de stocker un aussi grand int 
-  mot_de_passe_client VARCHAR(255) NOT NULL,
-  adresse_facturation_client VARCHAR(255) NOT NULL,
-  numero_carte_credit_client VARCHAR(255) NOT NULL, #VARCHAR car trop long en int
-  grade_fidelite VARCHAR(255) NOT NULL default 'Sans_Grade',
-  privilege INT default 0,
+  telephone_client VARCHAR(255) NOT NULL,  
   PRIMARY KEY (id_client)
 );
 
 CREATE TABLE commande (
   id_commande INT NOT NULL AUTO_INCREMENT,
   id_client INT NOT NULL,
-  nb_produit int not null,
   date_commande DATE NOT NULL,
-  prix_commande DECIMAL NOT NULL,
-  etat_commande VARCHAR(255) NOT NULL,
-  reduc_appliquee INT DEFAULT 0,
   PRIMARY KEY (id_commande)
 );
+
 create table produits_dans_commande (
   id_produits_dans_commande INT NOT NULL AUTO_INCREMENT,
   id_produit1 INT NOT NULL,
@@ -37,17 +26,31 @@ create table produits_dans_commande (
   reduc_appliquee INT DEFAULT 0,
   PRIMARY KEY (id_produits_dans_commande)
 );
+
+create table produit_vendu(
+	Id_vendu int not null auto_increment,
+    Id_produit int not null,
+    reduc_appliquee int default 0,
+    primary key (Id_vendu)
+    );
+
 CREATE TABLE produit (
-  id_produit INT NOT NULL AUTO_INCREMENT,
-  taille INT NOT NULL,
-  prix DECIMAL NOT NULL,
-  nombre_stock int NOT NULL,
-  reduc_appliquee INT DEFAULT 0,
-  PRIMARY KEY (id_produit)
+  Id INT NOT NULL AUTO_INCREMENT,
+  Categorie enum('Accesoire','Vêtement','Chaussure'),
+  Prix DECIMAL NOT NULL,
+  Taille INT,  
+  Stock int NOT NULL,
+  PRIMARY KEY (Id)
 );
 
-INSERT INTO produit (taille, prix, nombre_stock) VALUES
-(42,12,5),
-(43,17,4),
-(34,8,5),
-(48,12,6)
+INSERT INTO produit (Categorie,Taille, Prix, Stock) VALUES
+('Chaussure',42,12,5),
+('Chaussure',43,17,4),
+('Vêtement',34,8,5),
+('Vêtement',48,12,6);
+
+INSERT INTO produit (Categorie, Prix, Stock) VALUES
+('Accesoire',12,8),
+('Accesoire',10,3)
+
+
