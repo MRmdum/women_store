cbccb225b75676d0a98cd2819fc1a87686458678
Pierre Olivier COUDERT <pierreolivier.coudert@laposte.net>
2023-12-11 19:41:17 +0100

    Get value from tableview

--- src/main/java/com/example/demo1/Page2.java
+++ src/main/java/com/example/demo1/Page2.java
@@ -1,12 +1,16 @@
 package com.example.demo1;
 
+import com.example.demo1.sqlOperation.GeneralUtils;
 import com.example.demo1.sqlOperation.MysqlInterface;
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Page2 extends Application {
 
@@ -20,6 +24,7 @@ public class Page2 extends Application {
     @FXML
     protected void initialize(){
 
+        tableview.getItems().clear();
         TableView temp = new MysqlInterface().ReadData("Select * from produit");
         tableview.getColumns().addAll(temp.getColumns());
         tableview.setItems(temp.getItems());
@@ -37,5 +42,22 @@ public class Page2 extends Application {
         HelloApplication.setRoot("pagevente-view");
     }
 
+    @FXML
+    private void tableviewSelect(){
+
+        TableView.TableViewSelectionModel selectionModel = tableview.getSelectionModel();
+        ObservableList selectedCells = selectionModel.getSelectedCells();
+        TablePosition tablePosition = (TablePosition) selectedCells.get(0);
+        int colIndex = tableview.getColumns().indexOf(tableview.getFocusModel().getFocusedCell().getTableColumn());
+        int rowIndex = tableview.getSelectionModel().getSelectedIndex();
+
+        Object val = tablePosition.getTableColumn().getCellData(rowIndex);
+        var row =  tableview.getSelectionModel().getSelectedItems().get(0);
+        var row_val = row.toString().replace("[","").replace("]","").replace(" ","").split(",");
+
+        var row_eval = new GeneralUtils().isInt(row_val[0]) ? Double.parseDouble(row_val[0]) : row_val[0];
+        System.out.println("rowIndex " + rowIndex+" /colIndex "+colIndex+
+                " /val "+ val+" /row "+row+" /row0 "+ row_eval);
+    }
 
 }
---
+++ src/main/java/com/example/demo1/sqlOperation/GeneralUtils.java
@@ -0,0 +1,29 @@
+package com.example.demo1.sqlOperation;
+
+public class GeneralUtils {
+
+    public boolean isDouble(String strNum){
+
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Double.parseDouble(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+    public boolean isInt(String strNum){
+
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            int d = Integer.parseInt(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+}
